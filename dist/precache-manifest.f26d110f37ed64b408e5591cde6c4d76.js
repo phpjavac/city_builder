97302f241ca34881349a663e0be7a96fa3bf8447
SunMagic <1060854705@qq.com>
2021-01-14 17:08:01 +0800

    build

---
+++ dist/precache-manifest.f26d110f37ed64b408e5591cde6c4d76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db616cac3cae9b6ee61d",
+    "url": "/city_builder/css/app.6eaf02f9.css"
+  },
+  {
+    "revision": "9c7ffcaa52c00d4459b6",
+    "url": "/city_builder/css/chunk-4748f5a5.a7c10401.css"
+  },
+  {
+    "revision": "32a225db7a638669e41be9bc539919ae",
+    "url": "/city_builder/index.html"
+  },
+  {
+    "revision": "db616cac3cae9b6ee61d",
+    "url": "/city_builder/js/app.fb56ab2d.js"
+  },
+  {
+    "revision": "9c7ffcaa52c00d4459b6",
+    "url": "/city_builder/js/chunk-4748f5a5.650729c3.js"
+  },
+  {
+    "revision": "226e12747ce452226140",
+    "url": "/city_builder/js/chunk-vendors.a241658f.js"
+  },
+  {
+    "revision": "7c069b7b08e543da058a52a2b296fee4",
+    "url": "/city_builder/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/city_builder/robots.txt"
+  },
+  {
+    "revision": "a1fe2691674eeec31cee228641074797",
+    "url": "/city_builder/textures/01_130x66_130x230.png"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/city_builder',
+}
