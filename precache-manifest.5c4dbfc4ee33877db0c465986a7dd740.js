59693b49e499bf3700a307b099850376625b044d
SunMagic <1060854705@qq.com>
2021-01-14 16:57:21 +0800

    dist

---
+++ precache-manifest.5c4dbfc4ee33877db0c465986a7dd740.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a8b7a07919ecee46c8",
+    "url": "css/app.6eaf02f9.css"
+  },
+  {
+    "revision": "a7606cb7ff771a3d6953",
+    "url": "css/chunk-00ca0901.a7c10401.css"
+  },
+  {
+    "revision": "0aa9314e0b948de8cdf14a186f37218c",
+    "url": "index.html"
+  },
+  {
+    "revision": "50a8b7a07919ecee46c8",
+    "url": "js/app.b2e3b5a9.js"
+  },
+  {
+    "revision": "a7606cb7ff771a3d6953",
+    "url": "js/chunk-00ca0901.e35bde53.js"
+  },
+  {
+    "revision": "226e12747ce452226140",
+    "url": "js/chunk-vendors.a241658f.js"
+  },
+  {
+    "revision": "7c069b7b08e543da058a52a2b296fee4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a1fe2691674eeec31cee228641074797",
+    "url": "textures/01_130x66_130x230.png"
+  }
+]);
